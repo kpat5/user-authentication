4f5d70065762db337c34aeb3bf6b5d0d5289cbd2
Karan Singh Patrick <karanpatrick233@gmail.com>
2022-07-05 12:40:04 +0530

    initialize react

--- backend/controller.js
+++ backend/controller.js
@@ -2,3 +2,7 @@ const db = require("./databaseConn");
 const { signUp, login } = require("./verify");
 const validate = require("express-validator");
 const bcrypt = require("bcryptjs");
+
+async function signUp(req, req, next) {}
+
+const signUp = async;
